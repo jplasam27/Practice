e714f69b11f8b345e551c20fa6815b54dd5f1583
jplasam27 <69553651+jplasam27@users.noreply.github.com>
2020-10-17 03:06:11 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+# DATA TYPES
+from math import *
+
+# STRINGS
+name = "Jonathan Lasam"
+print(name)
+print(name.upper().isupper())
+print(len(name))
+print(name[0])
+print(name.index("tha"))
+print(name.replace("Lasam", "Pogi"))
+print(name)
+print(name.replace("Jonathan Lasam", "Pogi").upper())
+print(name.splitlines())
+print(name.split())
+print(name.lower())
+print(name.capitalize())
+print(name.casefold())
+
+# NUMBERS
+number_1 = 7
+print(number_1)
+print(str(number_1) + " ate 9")
+
+number_2 = -8
+print(abs(number_2))
+number_3 = pow(number_1, number_2)
+print(number_3)
+print(max(1, 2, 3, 4))
+print(round(3.7))
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(144))
+
+# GETTING INPUT
